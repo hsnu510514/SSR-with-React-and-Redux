f0cfdf76e6f000557607332bc8b020f82bd2202f
Sean <hsnu510514@gmail.com>
2019-07-17 15:21:17 +0800

    authReducer

--- src/client/reducers/index.js
+++ src/client/reducers/index.js
@@ -1,7 +1,8 @@
 import { combineReducers } from 'redux';
 import usersReducer from './usresReducer';
-
+import authReducer from './authReducer';
 
 export default combineReducers({
-  users: usersReducer
-})
+  users: usersReducer,
+  auth: authReducer
+});
